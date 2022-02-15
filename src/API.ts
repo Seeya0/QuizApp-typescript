6da09891b9814b57784a0cac914064d9575095b5
Seeya0 <vinlandtorfin@gmail.com>
2022-02-15 23:42:48 +0900

    App.tsxにロジックを入れた

--- src/API.ts
+++ src/API.ts
@@ -1,10 +1,29 @@
 import { shuffleArray } from "./utils";
 
-export const fetchQuiz = async (amount, difficulty) => {
+const DIFFICULTY = {
+    EASY: 'easy',
+    MEDIUM: 'medium',
+    HARD: 'hard'
+} as const;
+
+export type Difficulty = typeof DIFFICULTY[keyof typeof DIFFICULTY]
+
+export type Question = {
+    category: string,
+    correct_answer: string,
+    difficulty: string,
+    incorrect_answers: string[],
+    question: string,
+    type: string
+}
+
+export type QuestionState = Question & { answers: string[] }
+
+export const fetchQuiz = async (amount: number, difficulty: Difficulty): Promise<QuestionState[]> => {
     const endpoint = `https://opentdb.com/api.php?${amount}=10&difficulty=${difficulty}category=12&type=multiple`
     const response = await (await fetch(endpoint)).json();
 
-    return response.results.map((question) => ({
+    return response.results.map((question: Question) => ({
         ...question,
         answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
     }))
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,69 @@
 import { useState } from "react";
+import { fetchQuiz, Difficulty, QuestionState } from "./API";
+
+const QuestionAmount = 5
+
+export type userAnswersType = {
+  question: string,
+  userAnswer: string,
+  correct: boolean,
+  correctAnswer: string
+}
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<userAnswersType[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  const startQuiz = async () => {
+    setLoading(true);
+    setGameOver(false);
+    const newQuestions = await fetchQuiz(
+      QuestionAmount,
+      "medium"
+    );
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  }
+
+  const checkAnswer = (e: any) => {
+    if (!gameOver) {
+      //ユーザーの回答
+      const userAnswer = e.currentTarget.value;
+
+      //正解かどうかの判定
+      const correct = questions[number].correct_answer === userAnswer;
+
+      if (correct) {
+        setScore((prev) => prev + 1)
+      }
+
+      const answerObject = {
+        question: questions[number].question,
+        userAnswer,
+        correct,
+        correctAnswer: questions[number].correct_answer
+      }
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  }
+
+  const moveToQuestion = () => {
+    //最後の質問でない場合は次にすすむ
+    const nextQuestion = number + 1
+
+    if (nextQuestion === QuestionAmount) {
+      setGameOver(true)
+    } else {
+      setNumber(nextQuestion)
+    }
+  }
 
   return (
     <div className="App">
